11d7909a66c308cf77daee3e1740462efe33234d
arty1223 <65350351+arty1223@users.noreply.github.com>
2026-06-26 09:22:18 +0300

    update

--- app.py
+++ app.py
@@ -1,30 +1,59 @@
+# Created on: Jun 26, 2026
+#       Authors: Artyom Skibitskiy arty1223
+
 from os import mkdir, chdir, path, listdir, getcwd
 import sys
 import json
 
-missing_data = 0
+dimmTypes = [
+    "CAMM2",
+    "Mini-UDIMM",
+    "Mini-RDIMM",
+    "16b-SO-DIMM",
+    "SODIMM",
+    "DDIMM",
+    "SOCAMM2",
+    "CUDIMM",
+    "UDIMM",
+    "32b-SO-DIMM",
+    "RDIMM",
+    "Micro-DIMM",
+    "MRDIMM",
+    "SO-DIMM",
+    "72b-SO-CDIMM",
+    "LRDIMM",
+    "Reserved",
+    "72b-SO-UDIMM",
+    "Solder down",
+    "Mini-CDIMM",
+    "CSODIMM",
+    "72b-SO-RDIMM",
+]
+
 
 def parse_key_value_text(text):
     """Преобразует текст в формате 'ключ: значение' в словарь"""
     result = {}
-    for line in text.strip().split('\n'):
-        if ':' in line:
-            key, value = line.split(':', 1)
+    for line in text.strip().split("\n"):
+        if ":" in line:
+            key, value = line.split(":", 1)
             result[key.strip()] = value.strip()
     return result
 
-def get_value(dict_obj, key, default='NONE'):
+
+def get_value(dict_obj, key, default="NONE"):
     global missing_data
     value = dict_obj.get(key, default)
     if value == default:
         missing_data = True
     return value
 
+
 def parseFile(a):
-    content = ''
+    content = ""
     with open(a, "r") as file:
         # content = file.read()
-        line = ''
+        line = ""
         while True:
             # line = file.readline()
             # if not line:
@@ -37,7 +66,7 @@ def parseFile(a):
                 content += line
             except:
                 continue
-            
+
     content = content.split("""-------------------------------------------------------------
                          MEMORY MODULE
 -------------------------------------------------------------
@@ -55,20 +84,21 @@ def parseFile(a):
 """)
     dram_components = content[0][:-1:]
     spd_dump = content[1][:-1:]
-    
+
     memory_module = parse_key_value_text(memory_module)
     dram_components = parse_key_value_text(dram_components)
-    spd_dump_str = spd_dump.split('\n')
+    spd_dump_str = spd_dump.split("\n")
     for i in range(len(spd_dump_str)):
         spd_dump_str[i] = spd_dump_str[i].split()
         spd_dump_str[i].pop(0)
-        
+
     for i in range(len(spd_dump_str)):
-        spd_dump_str[i] = [int(x, base =16) for x in spd_dump_str[i]]
+        spd_dump_str[i] = [int(x, base=16) for x in spd_dump_str[i]]
 
     spd_dump = spd_dump_str.copy()
     spd_dump = [item for sublist in spd_dump for item in sublist]
     return [memory_module, dram_components, spd_dump]
+
 
 if __name__ == "__main__":
     directoryIn = "./thaiphoonDumps"
@@ -81,12 +111,12 @@ if __name__ == "__main__":
                 case "-i":
                     directoryIn = sys.argv[i + 1]
                     i += 2
-                case "-o":                    
+                case "-o":
                     directoryOut = sys.argv[i + 1]
                     i += 2
                 case _:
                     i += 1
-    try:                    
+    try:
         if path.exists(directoryOut):
             chdir(directoryOut)
         else:
@@ -97,7 +127,7 @@ if __name__ == "__main__":
         print("Недостаточно прав для перехода в директорию вывода или её создания")
         exit()
     chdir(root)
-    try:              
+    try:
         chdir(directoryIn)
         directoryIn = getcwd()
     except PermissionError:
@@ -111,77 +141,73 @@ if __name__ == "__main__":
     chdir(directoryIn)
     dumps = listdir()
     dumps = [i for i in dumps if i.endswith(".txt")]
-    
+
     fails = []
 
     binaries = {}
     for f in range(len(dumps)):
         file = dumps[f]
-        memory_module, dram_components, spd_dump = parseFile(f'{directoryIn}/{file}')
+        memory_module, dram_components, spd_dump = parseFile(f"{directoryIn}/{file}")
         # name = f'{memory_module['Architecture']} {memory_module['Manufacturer']} {memory_module['Part Number']} {memory_module['Capacity'].split('(')[0].replace(' ','',-1)} {memory_module['Speed Grade']} {dram_components['Part Number']}'
-        
-        architecture = get_value(memory_module, 'Architecture')
-        manufacturer = get_value(memory_module, 'Manufacturer')
-        part_number = get_value(memory_module, 'Part Number')
 
-        capacity = memory_module.get('Capacity', 'NONE')
-        if capacity == 'NONE':
+        architecture = get_value(memory_module, "Architecture")
+        manufacturer = get_value(memory_module, "Manufacturer")
+        part_number = get_value(memory_module, "Part Number")
+
+        capacity = memory_module.get("Capacity", "NONE")
+        if capacity == "NONE":
             missing_data = True
-            capacity = 'NONE'
+            capacity = "NONE"
         else:
-            capacity = capacity.split('(')[0].replace(' ', '', -1)
+            capacity = capacity.split("(")[0].replace(" ", "", -1)
 
-        speed_grade = get_value(memory_module, 'Speed Grade')
-        dram_part = get_value(dram_components, 'Part Number')
+        speed_grade = get_value(memory_module, "Speed Grade")
+        dram_part = get_value(dram_components, "Part Number")
 
         name = f"{architecture} {manufacturer} {part_number} {capacity} {speed_grade} {dram_part}"
-        
-        name += '.bin'
+
+        name += ".bin"
         result = name
         for i in name:
             if i in '<>:"/\\|?* ':
                 result = result.replace(i, "_")
         name = result
-        print(f'{name}, {f}, {round(f / (len(dumps)) * 100, 1)}%')
-        with open(directoryOut + '/' + name, "wb") as output:
+        print(f"{name}, {f}, {round(f / (len(dumps)) * 100, 1)}%")
+        with open(directoryOut + "/" + name, "wb") as output:
             output.write(bytearray(spd_dump))
         if missing_data:
             fails.append([file, name])
-        
+
         type = ""
-        if(str(architecture).find("SO-DIMM") > 0):            
-            type = "SO-DIMM"
-        elif(str(architecture).find("UDIMM") > 0):
-            type = "UDIMM"
-        elif(str(architecture).find("RDIMM") > 0):
-            type = "RDIMM"
-        
+        for i in dimmTypes:
+            if str(architecture).find(i) > 0:
+                type = i
+                break
         ddr = ""
-        if(str(architecture).find("DDR3") >= 0):
+        if str(architecture).find("DDR3") >= 0:
             ddr = "DDR3"
-        elif(str(architecture).find("DDR4") >= 0):
+        elif str(architecture).find("DDR4") >= 0:
             ddr = "DDR4"
-        elif(str(architecture).find("DDR5") >= 0):
+        elif str(architecture).find("DDR5") >= 0:
             ddr = "DDR5"
 
-        if type == '':
-            type = 'unsorted'
-        part_number = get_value(memory_module, 'Part Number')
-        dram_part = get_value(dram_components, 'Part Number')
+        if type == "":
+            type = "unsorted"
+        part_number = get_value(memory_module, "Part Number")
+        dram_part = get_value(dram_components, "Part Number")
         binaries[name] = {
-            "type":type,
-            "ddr":ddr,
+            "type": type,
+            "ddr": ddr,
             "architecture": architecture,
             "manufacturer": manufacturer,
             "part_number": part_number,
             "capacity": capacity,
             "speed_grade": speed_grade,
-            "dram_part": dram_part
+            "dram_part": dram_part,
         }
 
     # with open("../output.json", "w") as fp:
-    #     json.dump(binaries , fp) 
+    #     json.dump(binaries , fp)
     print(f"\nошибки {len(fails)}:")
     for i in fails:
         print(i[0], i[1])
-        
